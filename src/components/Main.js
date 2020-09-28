75ce83d61afdef1342438f20df047ef0c02a62ca
mojvaf <mvafa44@gmail.com>
2020-09-28 23:53:02 +0200

    added some styles

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,28 +1,21 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
 import Question from "./Question";
 import asian from "./image/asian.jpg";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 
 const Main = () => {
   return (
     <div>
-      <section>
-        <Container>
-          <Row>
-            <Col xs={11} className="app">
-              <Col>Prepare Children to Return to the Classroom</Col>
-              <img className="pic" src={asian} />
-              <Col></Col>
-            </Col>
-
-            <Col xs={1}>
-              <Question />
-            </Col>
-          </Row>
-        </Container>
-      </section>
-
-      <section></section>
+      <Grid container spacing={5}>
+        <Grid item xs={6}>
+          <Typography>should</Typography>
+          <img src={asian} className="pic " />
+        </Grid>
+        <Grid item xs={3}>
+          <Question />
+        </Grid>
+      </Grid>
     </div>
   );
 };
